42ab9c4ecf60682f541ba8af01ff1e743155b022
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-04-13 10:05:49 +0700

    interface, services paymentRecord

---
+++ DCT1205.Services/IPaymentRecordServices.cs
@@ -0,0 +1,26 @@
+﻿using DCT1205.Entity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCt1205.Services
+{
+    public interface IPaymentRecordServices
+    {
+        IEnumerable<PaymentRecord> GetAll();
+        PaymentRecord GetById(int id);
+        Task CreateByAsync(PaymentRecord record);
+        TaxYear GetTaxYearById(int id);
+        IEnumerable<SelectListItem> GetAllTaxYear();
+        decimal OvertimeHours(decimal hoursWorked, decimal contractualHours);
+        decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+        decimal OvertimeRate(decimal hourlyRate);
+        decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+        decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+        decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees);
+        decimal NetPay(decimal totalEarnings, decimal totalDeduction);
+    }
+}
---
+++ DCT1205.Services/implementation/PaymentRecordServices.cs
@@ -0,0 +1,109 @@
+﻿using DCT1205.Entity;
+using DCT1205.persistence;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCt1205.Services.Implementation
+{
+    public class PaymentRecordServices : IPaymentRecordServices
+    {
+        private readonly ApplicationDbContext _context;
+        private decimal overTimeHours;
+        private decimal contractualEarnings;
+        public PaymentRecordServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CreateByAsync(PaymentRecord record)
+        {
+            await _context.PaymentRecord.AddAsync(record);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll()
+        {
+            return _context.PaymentRecord
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            var ListTaxYear = _context.TaxYear.Select(t => new SelectListItem
+            {
+                Text = t.YearOfTax,
+                Value = t.Id.ToString()
+            });
+            return ListTaxYear;
+        }
+
+        public PaymentRecord GetById(int id)
+        {
+            return _context.PaymentRecord
+                .AsNoTracking()
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public TaxYear GetTaxYearById(int id)
+        {
+            return _context.TaxYear.FirstOrDefault(t => t.Id == id);
+        }
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction)
+        {
+            return totalEarnings - totalDeduction;
+        }
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours)
+        {
+            return overTimeHours * overtimeRate;
+        }
+
+        public decimal OvertimeHours(decimal hoursWorked, decimal contractualHours)
+        {
+            if (hoursWorked < contractualHours)
+            {
+                overTimeHours = 0.0m;
+            }
+            else if (hoursWorked > contractualHours)
+            {
+                overTimeHours = hoursWorked - contractualHours;
+            }
+            return overTimeHours;
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate)
+        {
+            return hourlyRate * 1.5m;
+        }
+
+        public decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees)
+        {
+            return tax + nic + studentLoanRepayment + unionFees;
+        }
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings)
+        {
+            return overtimeEarnings + contractualEarnings;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            if (hoursWorked < contractualHours)
+            {
+                contractualEarnings = hoursWorked * hourlyRate;
+            }
+            else
+            {
+                contractualEarnings = contractualHours * hourlyRate;
+            }
+            return contractualEarnings;
+        }
+    }
+}
